4c07360580e5b7288b9b5cc332e0ea25e9509e24
HyNuke <2063493+HyNuke@users.noreply.github.com>
2019-04-18 01:23:17 +0900

    Initial commit

---
+++ src/js/scroll.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    $("#scroll1").click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({scrollTop:$(this.hash).offset().top}, 500);
+    });
+});
+
+$(document).ready(function() {
+    $("#scroll2").click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({scrollTop:$(this.hash).offset().top}, 500);
+    });
+});
+
+$(document).ready(function() {
+    $("#scroll3").click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({scrollTop:$(this.hash).offset().top}, 500);
+    });
+});
+
+$(document).ready(function() {
+    $("#scroll4").click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({scrollTop:$(this.hash).offset().top}, 500);
+    });
+});
+
+$(document).ready(function() {
+    $("#scroll5").click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({scrollTop:$(this.hash).offset().top}, 500);
+    });
+});
